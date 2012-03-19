c7e9fe1c8688b62d37121579bc812002c1ac3b36
higgo <manicrider@gmail.com>
2012-03-19 22:21:50 +0000

    The one true commit

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -1,4 +1,3 @@
-/*global jQuery */
 /*!	
 * FitText.js 1.0
 *
@@ -8,37 +7,4 @@
 *
 * Date: Thu May 05 14:23:00 2011 -0600
 */
-
-(function( $ ){
-	
-	$.fn.fitText = function( kompressor, options ) {
-	    
-	    var settings = {
-        'minFontSize' : Number.NEGATIVE_INFINITY,
-        'maxFontSize' : Number.POSITIVE_INFINITY
-      };
-	
-			return this.each(function(){
-				var $this = $(this);              // store the object
-				var compressor = kompressor || 1; // set the compressor
-        
-        if ( options ) { 
-          $.extend( settings, options );
-        }
-        
-        // Resizer() resizes items based on the object width divided by the compressor * 10
-				var resizer = function () {
-					$this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-				};
-
-				// Call once to set.
-				resizer();
-				
-				// Call on resize. Opera debounces their resize by default. 
-      	$(window).resize(resizer);
-      	
-			});
-
-	};
-
-})( jQuery );
+(function($){$.fn.fitText=function(kompressor,options){var settings={minFontSize:Number.NEGATIVE_INFINITY,maxFontSize:Number.POSITIVE_INFINITY};return this.each(function(){var $this=$(this);var compressor=kompressor||1;if(options){$.extend(settings,options)}var resizer=function(){$this.css("font-size",Math.max(Math.min($this.width()/(compressor*10),parseFloat(settings.maxFontSize)),parseFloat(settings.minFontSize)))};resizer();$(window).resize(resizer)})}})(jQuery);
