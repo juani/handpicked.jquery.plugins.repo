46cebb6e2c9d8c45eb5556f0fe64308640130cd4
David Higgins <david.higgins.tech@gmail.com>
2012-06-28 14:32:52 +0100

    The one true commit

--- browser/index.php
+++ browser/index.php
@@ -1,11 +1,56 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title>listDirectory</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link rel="stylesheet" type="text/css" href="listDirectory/assets/css/style.css" />
-	<script src="listDirectory/assets/js/jquery-1.3.2.min.js" type="text/javascript"></script>
+<meta charset="utf-8">
+<title>The Handpicked jQuery Plugins Repository. :: File Browser :: Downloads</title>
+
+<link rel="dns-prefetch" href="//u-n-i.co">
+<link rel="dns-prefetch" href="//higg.in">
+<link rel="dns-prefetch" href="//davidhiggins.me">
+<link rel="dns-prefetch" href="//codepirat.es">
+<link rel="dns-prefetch" href="//edanhewitt.com">
+<link rel="dns-prefetch" href="//r3versin.com">
+
+<meta name="description" content="The Handpicked jQuery Plugins Repository. :: File Browser :: Downloads">
+<meta name="author" content="David Higgins">
+<meta name="keywords" content="jquery,plugins,handpicked,jquery-plugins,plugins-jquery,plugin-collection,collection,best,best-plugins,good-jquery-plugins,jQuery,jquery-handpicked,david-higgins,higgins" />
+<meta name="robots" content="index,follow" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="icon" type="image/x-icon" href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="/>
+<link rel="humans" href="http://iwantaneff.in/humans.txt" />
+<link rel="image_src" href="http://iwantaneff.in/repo/img/header.png" />
+
+<meta property="og:title" content="The Handpicked jQuery Plugins Repository. :: File Browser :: Downloads" />
+<meta property="og:url" content="http://iwantaneff.in/repo/browser/" />
+<meta property="og:image" content="http://iwantaneff.in/repo/img/header.png" />
+<meta property="og:site_name" content="The Handpicked jQuery Plugins Repository. :: File Browser :: Downloads" />
+
+<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+<style>::-moz-selection{ background: #000; color: white; text-shadow: none; }::selection { background:#000; color:white; text-shadow: none; }</style>
+<style>
+* {
+    resize: none;
+    border: none;
+    outline: none;
+    text-decoration: none;
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
+    font-family: Verdana, Arial, Helvetica
+}
+</style>
+<link href="http://iwantaneff.in/custom.webkit.scrollbar.css" rel="stylesheet" type="text/css">
+
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<link rel="stylesheet" type="text/css" href="listDirectory/assets/css/style.css" />
+<script src="listDirectory/assets/js/jquery-1.3.2.min.js" type="text/javascript"></script>
+
 </head>
+
 <body>
 <div class="dir">
 <?php
